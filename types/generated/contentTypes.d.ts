9ab46c3d5ca294c92d3a8f263ce5f6256b78da62
Nazar Yanchyshyn <nazaryanchyshyn@ukr.net>
2024-04-02 11:33:00 +0300

    add new fields

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -512,6 +512,8 @@ export interface PluginContentReleasesRelease extends Schema.CollectionType {
   attributes: {
     name: Attribute.String & Attribute.Required;
     releasedAt: Attribute.DateTime;
+    scheduledAt: Attribute.DateTime;
+    timezone: Attribute.String;
     actions: Attribute.Relation<
       'plugin::content-releases.release',
       'oneToMany',
@@ -845,6 +847,17 @@ export interface ApiArticleArticle extends Schema.CollectionType {
     >;
     blocks: Attribute.DynamicZone<
       ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
+    >;
+    tag: Attribute.String;
+    recomend: Attribute.Relation<
+      'api::article.article',
+      'oneToMany',
+      'api::article.article'
+    >;
+    article: Attribute.Relation<
+      'api::article.article',
+      'manyToOne',
+      'api::article.article'
     >;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
